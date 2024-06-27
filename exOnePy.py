c4daf8c83b6d40025c9d81b9c1b35ceba2b973c8
Carmin Ilarraz <144161912+Carmin365@users.noreply.github.com>
2024-06-27 07:52:24 -0300

    Update exOnePy.py

--- exOnePy.py
+++ exOnePy.py
@@ -1,6 +1,9 @@
 # This is an example that presents the function of the print() command.  
 # Este é um exemplo que apresenta a função do comando print().
 
-print("Hello World!") # Show the message "Hello World!". # Mostra a mensagem "Olá mundo!"
-number = 10 # Declaration of the variable 'number' and assigning the value '10' to it. # Declaração da variável 'number' e atribuição do valor '10' à ela. 
-print(number) # Shows the value of the number variable. # Mostra o valor da variável "numero".
+# Line 1 >> Show the message "Hello World!". # Mostra a mensagem "Olá mundo!"
+# Line 2 >> Declaration of the variable 'number' and assigning the value '10' to it. # Declaração da variável 'number' e atribuição do valor '10' à ela. 
+# Line 3 >> Shows the value of the number variable. # Mostra o valor da variável "numero".
+print("Hello World!") 
+number = 10 
+print(number) 
