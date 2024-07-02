94b3e0efc782a68c0be3ec133d4da14f95923f9e
Carmin Ilarraz <144161912+Carmin365@users.noreply.github.com>
2024-07-02 17:47:37 -0300

    Update exTwoPy.py

--- exTwoPy.py
+++ exTwoPy.py
@@ -1,10 +1,16 @@
 # This is an example that presents the use of variables and data types.
 # Este é um exemplo que apresenta o uso de Variáveis e Tipos de Dados.
 
-name = "Paulo" # Declaration of the variable "name" of type string and assigning a value to it. # Declaração da variável "nome" do tipo string e atribuição de um valor à ela.
-age = 35    # Declaration of the variable "age" of type integer and assigning a value to it. # Declaração da variável "idade" do tipo inteiro e atribuição de um valor à ela.
-is_active = True # Declaration of the variable "is_active" of boolean type and assigning a value to it. # Declaração da variável "está_ativo" do tipo booleano e atribuição de um valor à ela.
+# Line 1 >> Declaration of the variable "name" of type string and assigning a value to it. # Declaração da variável "nome" do tipo string e atribuição de um valor à ela.
+# Line 2 >> Declaration of the variable "age" of type integer and assigning a value to it. # Declaração da variável "idade" do tipo inteiro e atribuição de um valor à ela.
+# Line 3 >> Declaration of the variable "is_active" of boolean type and assigning a value to it. # Declaração da variável "está_ativo" do tipo booleano e atribuição de um valor à ela.
+name = "Paulo" 
+age = 35    
+is_active = True
 
-print(name) # The function print(name) displays the value stored inside the parentheses in the console. # A função print(nome) exibe no console, o valor armazenado dentro dos parênteses.
-print(age) # The function print(age) displays the value stored inside the parentheses in the console. # A função print(idade) exibe no console, o valor armazenado dentro dos parênteses. 
-print(is_active) # The function print(is_active) displays the value stored inside the parentheses in the console. # A função print(está_ativo) exibe no console, o valor armazenado dentro dos parênteses.
+# Line 4 >> The function print(name) displays the value stored inside the parentheses in the console. # A função print(nome) exibe no console, o valor armazenado dentro dos parênteses.
+# Line 5 >> The function print(age) displays the value stored inside the parentheses in the console. # A função print(idade) exibe no console, o valor armazenado dentro dos parênteses. 
+# Line 6 >> The function print(is_active) displays the value stored inside the parentheses in the console. # A função print(está_ativo) exibe no console, o valor armazenado dentro dos parênteses.
+print(name) 
+print(age) 
+print(is_active) 
