9778830c60f75f00722f08d6e60711881010ae1a
Carmin Ilarraz <144161912+Carmin365@users.noreply.github.com>
2024-07-02 17:55:19 -0300

    Update exThreePy.py

--- exThreePy.py
+++ exThreePy.py
@@ -1,14 +1,22 @@
 # This is an example that presents the use of mathematical operators in the Python language. 
 # Este é um exemplo que apresenta o uso dos operadores matemáticos na linguagem Python. 
 
-soma  = 10 + 10   # Declaration of the variable 'sum' and assigning value to it. # Declaração da variável 'soma' e atribuição de valor a ela.
-subtracao = 20 - 5  # Declaration of the variable 'subtraction' and assigning value to it. # Declaração da variável 'subtração' e atribuição de valor a ela.
-multiplicacao = 8 * 2  # Declaration of the variable 'multiplication' and assigning value to it. # Declaração da variável 'multiplicação' e atribuição  de valor a ela. 
-divisao = 100 / 2  # Declaration of the variable 'division' and assigning value to it. # Declaração da variável 'multiplicação' e atribuição de valor a ela.
+# Line 1 >> Declaration of the variable 'sum' and assigning value to it. # Declaração da variável 'soma' e atribuição de valor a ela.
+# Line 2 >> Declaration of the variable 'subtraction' and assigning value to it. # Declaração da variável 'subtração' e atribuição de valor a ela.
+# Line 3 >> Declaration of the variable 'multiplication' and assigning value to it. # Declaração da variável 'multiplicação' e atribuição  de valor a ela. 
+# Line 4 >> Declaration of the variable 'division' and assigning value to it. # Declaração da variável 'multiplicação' e atribuição de valor a ela.
+soma  = 10 + 10   
+subtracao = 20 - 5  
+multiplicacao = 8 * 2  
+divisao = 100 / 2  
 
-print(soma)  # The function print(sum) displays the value inside the parentheses in the console. # A função print(soma) exibe no console o valor que está dentro dos parênteses.
-print(subtracao) # The function print(subtraction) displays the value inside the parentheses in the console. # A função print(subtração) exibe no console o valor que está dentro dos parênteses.
-print(multiplicacao) # The function print(multiplication) displays the value inside the parentheses in the console. # A função print(multiplicação) exibe no console o valor que está dentro dos parênteses.
-print(divisao) # The function print(division) displays the value inside the parentheses in the console. # A função print(divisão) exibe no console o valor que está dentro dos parênteses.
+# Line 5 >> The function print(sum) displays the value inside the parentheses in the console. # A função print(soma) exibe no console o valor que está dentro dos parênteses.
+# Line 6 >> The function print(subtraction) displays the value inside the parentheses in the console. # A função print(subtração) exibe no console o valor que está dentro dos parênteses.
+# Line 7 >> The function print(multiplication) displays the value inside the parentheses in the console. # A função print(multiplicação) exibe no console o valor que está dentro dos parênteses.
+# Line 8 >> The function print(division) displays the value inside the parentheses in the console. # A função print(divisão) exibe no console o valor que está dentro dos parênteses.
+print(soma)  
+print(subtracao) 
+print(multiplicacao) 
+print(divisao) 
 
 
