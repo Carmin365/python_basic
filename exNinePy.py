4a0e64a846b731776a5ae05fbe958da1177a5179
Carmin Ilarraz <144161912+Carmin365@users.noreply.github.com>
2024-07-03 20:25:00 -0300

    Update exNinePy.py

--- exNinePy.py
+++ exNinePy.py
@@ -1,12 +1,19 @@
 # This program exemplifies the use of different functions to manipulate strings 
 # # Este programa exemplifica o uso de diferentes funções para manipular strings.   
 
-text = "This is an example of manipulation of strings." # Defines a variable called text # Define uma variável chamada texto. 
+# Line 1 >> Defines a variable called text # Define uma variável chamada texto.
+text = "This is an example of manipulation of strings."  
 
-text_capital = text.upper() # Convert to capital letters # Converte para maiúsculas
-text_tiny = text.lower() # Convert to lowercase # Convert para minúsculas
-text_divided = text.split(" ") # Divide into a list by spaces # Divide em uma lista por espaços.
-print(text_capital) # Displays the content of the variable in the console # Exibe o conteúdo da variável no console.
-print(text_tiny) # Displays the content of the variable in the console # Exibe o conteúdo da variável no console.
-print() # Print() without any arguments, inserts a line break, which moves the cursor of the next line in the console # Print() sem nenhum argumento, insere uma quebra de linha, o que move o cursor para a próxima linha no console.  
+# Line 2 >> Convert to capital letters # Converte para maiúsculas
+# Line 3 >> Convert to lowercase # Convert para minúsculas
+# Line 4 >> Divide into a list by spaces # Divide em uma lista por espaços.
+# Line 5 >> Displays the content of the variable in the console # Exibe o conteúdo da variável no console.
+# Line 6 >> Displays the content of the variable in the console # Exibe o conteúdo da variável no console.
+# Line 7 >> Print() without any arguments, inserts a line break, which moves the cursor of the next line in the console # Print() sem nenhum argumento, insere uma quebra de linha, o que move o cursor para a próxima linha no console.
+text_capital = text.upper()  
+text_tiny = text.lower()  
+text_divided = text.split(" ")  
+print(text_capital) 
+print(text_tiny) 
+print()    
 
