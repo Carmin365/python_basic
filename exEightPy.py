15ee54c5a07b94a8816906f3f9d171888c42c450
Carmin Ilarraz <144161912+Carmin365@users.noreply.github.com>
2024-07-03 20:20:06 -0300

    Update exEightPy.py

--- exEightPy.py
+++ exEightPy.py
@@ -1,10 +1,15 @@
 # This program exemplifies the use of Dictionaries in the Python language 
 # Este programa exemplifica o uso de Dicionários na linguagem Python
 
-person = {"name": "Felipa", "age": 39, "city": "Flores"} # Here 'person' is the dictionary # Aqui 'pessoa' é o Dicionário.
+# Line 1 >> # Here 'person' is the dictionary # Aqui 'pessoa' é o Dicionário.
+person = {"name": "Felipa", "age": 39, "city": "Flores"} 
 
-print(person["name"]) # Accesses the value of the key "name" (Felipa) # Acessa o valor da chave "nome" (Fulano)
-person["city"] = "Triunfo" # Modify the value of the key "city" # Modifica o valor da chave "cidade"
-for key, value in person.items(): # Starts a for loop |  # Inicia um loop for | Defines two variables to store the iterated elements | Define duas varáveis para armazenar os elementos iterados. | Calls the method items() of the dictionaries person | Chama o método items() do dicionário pessoa.     
-    print(key, ": ", value) # Print prints these values to the console | # Print imprime estes valores no console.
+# Line 2 >> Accesses the value of the key "name" (Felipa) # Acessa o valor da chave "nome" (Fulano)
+# Line 3 >> Modify the value of the key "city" # Modifica o valor da chave "cidade"
+# Line 4 >> Starts a for loop |  # Inicia um loop for | Defines two variables to store the iterated elements | Define duas varáveis para armazenar os elementos iterados. | Calls the method items() of the dictionaries person | Chama o método items() do dicionário pessoa.
+# Line 5 >> Print prints these values to the console | # Print imprime estes valores no console.
+print(person["name"])  
+person["city"] = "Triunfo"  
+for key, value in person.items():       
+    print(key, ": ", value) 
 
