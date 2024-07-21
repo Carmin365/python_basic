143a524e23d381cd0c858c7438031faaf82b5f42
Carmin Ilarraz <144161912+Carmin365@users.noreply.github.com>
2024-07-20 21:15:13 -0300

    Update exSixPy.py

--- exSixPy.py
+++ exSixPy.py
@@ -1,7 +1,7 @@
 # This program exemplifies the use of functions in the Python language
 # Este programa exemplifica o uso das funções na linguagem Python 
 
-# Line 1 >> Defines the word 'add', and the content inside the parentheses are two parameters that are being assigned to the function. The colon (:) after closing the parentheses signals the beginning of the function body. # Define a palavra 'somar', e o conteúdo que está dentro dos parênteses são dois parâmetros que estão sendo atribuídos à função. Os dois pontos (:) após o fechamento dos parênteses, sinalizam para o começo do corpo da função.
+# Line 1 >> Defines the function 'add', and the content inside the parentheses are two parameters that are being assigned to the function. The colon (:) after closing the parentheses signals the beginning of the function body. # Define a função 'somar', e o conteúdo que está dentro dos parênteses são dois parâmetros que estão sendo atribuídos à função. Os dois pontos (:) após o fechamento dos parênteses, sinalizam para o começo do corpo da função.
 # Line 2 >> Declaration of variable 'result' and assigning two values together with the sum of them. The result of the operation is stored in the 'result' variable. # Declaração da variável 'resultado' e atribuição de dois valores juntamente com a soma dos mesmos. O resultado da operação é armazenado na variável 'resultado'.
 # Line 3 >> Returns the value of the variable 'result' out of the function. # Retorna o valor da variável 'resultado' para fora da função.
 def somar(number_one, number_two): 
