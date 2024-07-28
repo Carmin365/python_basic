edac573990793487c807b855a6a3791166d75574
Carmin Ilarraz <144161912+Carmin365@users.noreply.github.com>
2024-07-28 08:21:18 -0300

    Update exOnePy.py

--- exOnePy.py
+++ exOnePy.py
@@ -3,7 +3,7 @@
 
 # Line 1 >> Shows the message "Hello World!". # Mostra a mensagem "Olá mundo!"
 # Line 2 >> Declaration of the variable 'num' and assigning the value '10' to it. # Declaração da variável 'num' e atribuição do valor '10' à ela. 
-# Line 3 >> Shows the value of the num variable. # Mostra o valor da variável "num".
+# Line 3 >> Shows the value of the num variable. # Mostra o valor da variável 'num'.
 print("Hello World!") 
 num = 10 
 print(num) 
