a8d6daeb312881fb634a7b59b302b7e760774335
Carmin Ilarraz <144161912+Carmin365@users.noreply.github.com>
2024-07-28 08:23:03 -0300

    Update exSixPy.py

--- exSixPy.py
+++ exSixPy.py
@@ -4,8 +4,8 @@
 # Line 1 >> Defines the function 'add', and the content inside the parentheses are two parameters that are being assigned to the function. The colon (:) after closing the parentheses signals the beginning of the function body. # Define a função 'somar', e o conteúdo que está dentro dos parênteses são dois parâmetros que estão sendo atribuídos à função. Os dois pontos (:) após o fechamento dos parênteses, sinalizam para o começo do corpo da função.
 # Line 2 >> Declaration of variable 'result' and assigning two values together with the sum of them. The result of the operation is stored in the 'result' variable. # Declaração da variável 'resultado' e atribuição de dois valores juntamente com a soma dos mesmos. O resultado da operação é armazenado na variável 'resultado'.
 # Line 3 >> Returns the value of the variable 'result' out of the function. # Retorna o valor da variável 'resultado' para fora da função.
-def somar(number_one, number_two): 
-    resultado = number_one + number_two    
+def somar(num_one, num_two): 
+    resultado = num_one + num_two    
     return resultado  
 
     # Line 4 >> Declaration of variable 'sum_total' and assigning a value to it. # Declaração da variável 'soma_total' e atribuição de um valor à ela.
