39d034050576ac8175dadccb10716e6e6827ad8b
Carmin Ilarraz <144161912+Carmin365@users.noreply.github.com>
2024-07-28 08:12:27 -0300

    Update exOnePy.py

--- exOnePy.py
+++ exOnePy.py
@@ -5,5 +5,5 @@
 # Line 2 >> Declaration of the variable 'number' and assigning the value '10' to it. # Declaração da variável 'number' e atribuição do valor '10' à ela. 
 # Line 3 >> Shows the value of the number variable. # Mostra o valor da variável "numero".
 print("Hello World!") 
-number = 10 
-print(number) 
+num = 10 
+print(num) 
