fe020155b42cd2a8f46ba4ea473d0c7a02facce3
Carmin Ilarraz <144161912+Carmin365@users.noreply.github.com>
2024-08-14 14:30:10 -0300

    Update exSevenPy.py

--- exSevenPy.py
+++ exSevenPy.py
@@ -1,12 +1,5 @@
-# This program exemplifies the use of lists in the Python language
-# Este programa exemplifica o uso de listas na linguagem Python
-
-# Line 1 >> Declaração de variável e atribuição de valor à mesma, neste caso uma lista. # Variable declaration and value assignment, in this case a list.
 fruits = ["orange", "melon", "blackberry"]
 
-# Line 2 >> # Accesses the first element (orange) # Acessa o primeiro elemento (laranja)
-# Line 3 >> # List slicing (melon, blackberry) # Fatiamento da lista (melão, amora)
-# Line 4 >> # Adds a new element (grape) # Adiciona um novo elemento (uva)
 print(fruits[0]) 
 print(fruits[1:3]) 
 fruits.append("uva") 
