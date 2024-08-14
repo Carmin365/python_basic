79fd174f0d28a9c1916e5ac5055153d6e71db175
Carmin Ilarraz <144161912+Carmin365@users.noreply.github.com>
2024-08-14 14:39:11 -0300

    Update exTwoPy.py

--- exTwoPy.py
+++ exTwoPy.py
@@ -1,17 +1,6 @@
-# This is an example that presents the use of variables and data types.
-# Este é um exemplo que apresenta o uso de Variáveis e Tipos de Dados.
-
-# Line 1 of the code >> Declaration of the variable "name" of type string and assigning a value to it. # Declaração da variável "nome" do tipo string e atribuição de um valor à ela.
-# Line 2 of the code >> Declaration of the variable "age" of type integer and assigning a value to it. # Declaração da variável "idade" do tipo inteiro e atribuição de um valor à ela.
-# Line 3 of the code >> Declaration of the variable "is_active" of boolean type and assigning a value to it. # Declaração da variável "está_ativo" do tipo booleano e atribuição de um valor à ela.
-
 name = "Paulo" 
 age = 35    
 is_active = True
-
-# Line 4 of the code >> The function print(name) shows the value stored inside the parentheses in the console. # A função print(nome) mostra no console, o valor armazenado dentro dos parênteses.
-# Line 5 of the code >> The function print(age) shows the value stored inside the parentheses in the console. # A função print(idade) mostra no console, o valor armazenado dentro dos parênteses. 
-# Line 6 of the code >> The function print(is_active) shows the value stored inside the parentheses in the console. # A função print(está_ativo) mostra no console, o valor armazenado dentro dos parênteses.
 
 print(name) 
 print(age) 
