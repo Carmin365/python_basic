8bbb827169c5aed320d131bd5124a732aa23a186
Carmin Ilarraz <144161912+Carmin365@users.noreply.github.com>
2024-08-14 14:37:32 -0300

    Update exThreePy.py

--- exThreePy.py
+++ exThreePy.py
@@ -1,19 +1,8 @@
-# This is an example that presents the use of mathematical operators in the Python language. 
-# Este é um exemplo que apresenta o uso dos operadores matemáticos na linguagem Python. 
-
-# Line 1 >> Declaration of the variable 'sum' and assigning value to it. # Declaração da variável 'soma' e atribuição de valor a ela.
-# Line 2 >> Declaration of the variable 'subtraction' and assigning value to it. # Declaração da variável 'subtração' e atribuição de valor a ela.
-# Line 3 >> Declaration of the variable 'multiplication' and assigning value to it. # Declaração da variável 'multiplicação' e atribuição  de valor a ela. 
-# Line 4 >> Declaration of the variable 'division' and assigning value to it. # Declaração da variável 'multiplicação' e atribuição de valor a ela.
 soma  = 10 + 10   
 subtracao = 20 - 5  
 multiplicacao = 8 * 2  
 divisao = 100 / 2  
 
-# Line 5 >> The function print(sum) shows the value inside the parentheses in the console. # A função print(soma) mostra no console o valor que está dentro dos parênteses.
-# Line 6 >> The function print(subtraction) shows the value inside the parentheses in the console. # A função print(subtração) mostra no console o valor que está dentro dos parênteses.
-# Line 7 >> The function print(multiplication) shows the value inside the parentheses in the console. # A função print(multiplicação) mostra no console o valor que está dentro dos parênteses.
-# Line 8 >> The function print(division) shows the value inside the parentheses in the console. # A função print(divisão) mostra no console o valor que está dentro dos parênteses.
 print(soma)  
 print(subtracao) 
 print(multiplicacao) 
